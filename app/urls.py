1219c1ea11caa7a9c44421b079e28ae18b0b9e40
rjraju143 <bangarraju1152@gmail.com>
2024-02-13 06:10:39 +0000

    fix path error

--- app/urls.py
+++ app/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import test_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', {"message": "hello world"}),
+    path('test/', test_view),
 ]
+
+
